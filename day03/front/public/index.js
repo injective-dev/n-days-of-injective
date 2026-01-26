a2b695f16533f22e84aaa4619b7286c338cc3d81
Brendan Graetz <bguiz@users.noreply.github.com>
2026-01-26 13:06:03 +0800

    feat: impl fetching SC details API and wallet connection action:
    Signed-off-by: Brendan Graetz <bguiz@users.noreply.github.com>

--- day03/front/public/index.js
+++ day03/front/public/index.js
@@ -1,16 +1,28 @@
-import { createPublicClient, http } from 'viem';
-import { mainnet } from 'viem/chains';
+import { createWalletClient, custom } from 'viem';
+import { injectiveTestnet } from 'viem/chains';
 
-console.log(mainnet);
+console.log(injectiveTestnet);
+
+const smartContractDetails = await getJson('/api/smart-contract');
+console.log(smartContractDetails);
+const { rpc, address, abi } = smartContractDetails;
 
 const btnWallet = document.querySelector('#btnWallet');
 const txtWallet = document.querySelector('#txtWallet');
 let stateWallet = {
-    n: 0,
+    client: undefined,
+    address: undefined,
 };
-btnWallet.addEventListener('click', (_event) => {
-    stateWallet.n += 1;
-    txtWallet.innerHTML = `btnWallet clicked: ${stateWallet.n}`;
+btnWallet.addEventListener('click', async (_event) => {
+    const result = await connectEvmWallet();
+    if (result.ok) {
+        const { client, address } = result;
+        stateWallet = { client, address };
+        console.log(stateWallet);
+        txtWallet.innerHTML = `Connected to wallet, with account address: ${address}`;
+    } else {
+        txtWallet.innerHTML = `Error: ${result.error}`;
+    }
 });
 
 const btnRead = document.querySelector('#btnRead');
@@ -32,3 +44,50 @@ btnWrite.addEventListener('click', (_event) => {
     stateWrite.n += 1;
     txtWrite.innerHTML = `btnWrite clicked: ${stateWrite.n}`;
 });
+
+async function getJson(url) {
+    const fetchResponse = await fetch(url);
+    if (!fetchResponse.ok) {
+        throw new Error(`HTTP error! status: ${fetchResponse.status}`);
+    }
+    const data = await fetchResponse.json();
+    return data;
+}
+
+async function connectEvmWallet() {
+    if (typeof window?.ethereum === 'undefined') {
+        return {
+            ok: false,
+            error: 'no injected web3 provider detected'
+        };
+    }
+
+    let client;
+    try {
+        client = createWalletClient({
+            chain: injectiveTestnet,
+            transport: custom(window.ethereum),
+        });
+    } catch (ex) {
+        return {
+            ok: false,
+            error: 'unable to initialise wallet client'
+        };
+    }
+
+    let address;
+    try {
+        [address] = await client.requestAddresses();
+    } catch (ex) {
+        return {
+            ok: false,
+            error: 'unable to obtain account details'
+        };
+    }
+
+    return {
+        ok: true,
+        address,
+        client,
+    };
+}
